8cacabfc64606daf937d68af1f98d939b7312730
JonathanDmS <al2193041228@azc.uam.mx>
2024-08-11 13:03:48 -0600

    ventanaGTK.c
    Edit event button

--- ventanasgtk.c
+++ ventanasgtk.c
@@ -1,8 +1,8 @@
 #include <gtk/gtk.h>
 
-
-static void print_hello (GtkWidget *widget, gpointer data) {
-    g_print ("Hello World from GTK3\n");
+//event to close window on click
+static void on_button_clicked (GtkWidget *widget, gpointer window) {
+    gtk_window_close(GTK_WINDOW(window));
 }
 
 static void activate (GtkApplication *app, gpointer user_data) {
@@ -11,10 +11,10 @@ static void activate (GtkApplication *app, gpointer user_data) {
 
     window = gtk_application_window_new (app);
     gtk_window_set_title (GTK_WINDOW (window), "Window");
-    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+    gtk_window_set_default_size (GTK_WINDOW (window), 500, 500);
 
-    button = gtk_button_new_with_label ("Hello World");
-    g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+    button = gtk_button_new_with_label ("Close");
+    g_signal_connect (button, "clicked", G_CALLBACK (on_button_clicked), NULL);
     gtk_container_add (GTK_CONTAINER (window), button);
     gtk_widget_show (button);
     gtk_window_present (GTK_WINDOW (window));
